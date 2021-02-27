9cb8d0aaf3c9cb5dbd59724cbe17d4b026d7d943
Matheus Felipe Bosa <matheusbozza@gmail.com>
2021-02-27 18:37:04 -0300

    Creating account associated to an user

--- lib/payments_web/views/users_view.ex
+++ lib/payments_web/views/users_view.ex
@@ -1,14 +1,23 @@
 defmodule PaymentsWeb.UsersView do
-  alias Payments.User
+  alias Payments.{User, Account}
 
-  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname, email: email}}) do
+  def render("create.json", %{
+        user: %User{
+          account: %Account{id: account_id, balance: balance},
+          id: id, name: name, nickname: nickname, email: email
+        }
+      }) do
     %{
       message: "User created",
       user: %{
         id: id,
         name: name,
         nickname: nickname,
-        email: email
+        email: email,
+        account: %{
+          id: account_id,
+          balance: balance
+        }
       }
     }
   end
